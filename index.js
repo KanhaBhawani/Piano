7d90c32b0e85370004c46209956e585b045e94d9
Kanha Bhawani <ghanishtsinghal@gmail.com>
2021-10-11 12:10:21 +0530

    added sound

--- index.js
+++ index.js
@@ -1,53 +1,43 @@
-// alert("check if keys are pressed or not");
-isPressed_192 = false; // `
-isPressed_9 = false; // tab
-isPressed_81 = false; // q
-isPressed_87 = false; // w
-isPressed_69 = false; // e
-isPressed_82 = false; // r
-isPressed_84 = false; // t
-isPressed_89 = false; // y
-isPressed_85 = false; // u
-isPressed_73 = false; // i
-isPressed_79 = false; // o
-isPressed_80 = false; // p
-isPressed_219 = false; // [
-isPressed_221 = false; // ]
-isPressed_220 = false; // \
-isPressed_13 = false; // enter
-isPressed_49 = false; // 1
-isPressed_50 = false; // 2
-// isPressed_51 = false; // 3
-isPressed_52 = false; // 4
-isPressed_53 = false; // 5
-isPressed_54 = false; // 6
-// isPressed_55 = false; // 7
-isPressed_56 = false; // 8
-isPressed_57 = false; // 9
-// isPressed_48 = false; // 0
-isPressed_189 = false; // -
-isPressed_187 = false; // =
-isPressed_8 = false; // backspace
+var i = 3;
+var keys = ['C','D','E','F','G','A','B'];
 
+/* ------------------------------------------------- */
 
-
-$(document).keydown(function(event){
-    // console.log(event.key);
+$(document).keydown(function (event) {
     var x = event.charCode || event.keyCode;
-    console.log(x);
-    startPlay(x);
+    $("." + x).addClass("button-active");
+    var Id = $(("." + x)).attr('id');
+    Id = Id.slice(1);
+    var note = keys[(Id-1)%7];
+    var octave = Math.floor((Id-1)/7) + i;
+    console.log(note);
+    console.log(octave);
+    startPlay(note,octave);
 });
 
-$(document).keyup(function(event){
+$(document).keyup(function (event) {
     var x = event.charCode || event.keyCode;
-    stopPlay(x);
+    $("." + x).removeClass("button-active");
+
+    // stopPlay(note,octave);
 });
 
-function startPlay(Key){
-    $("." + Key).addClass("button-active");
+$('button').mousedown(function(){
+    var Id = $(this).attr('id');
+    $('#'+Id).addClass("button-active");
+});
+
+$('button').mouseup(function(){
+    var Id = $(this).attr('id');
+    $('#'+Id).removeClass("button-active");
+});
+
+/* ------------------------------------------------- */
+
+function startPlay(note,octave) {
+    var piano = Synth.createInstrument('piano');
+    piano.play(note, octave, 2.5); 
 }
 
-function stopPlay(Key){
-    // console.log(Key);
-    $("." + Key).removeClass("button-active");
+function stopPlay(note) {
 }

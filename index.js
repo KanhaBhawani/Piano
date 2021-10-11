f6727ab989f0a73c7858e967c1b207667d36b7df
Kanha Bhawani <ghanishtsinghal@gmail.com>
2021-10-11 15:44:42 +0530

    updating image and keys

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-var i = 3;
-var keys = ['C','D','E','F','G','A','B'];
+var start_octave = 3;
+var piano = Synth.createInstrument('piano');
+var keys_long = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+var keys_short = ['C#', 'D#', 'F#', 'G#', 'A#'];
+var keyboard_size = 15;
 
+for (let index = 1; index <= 15; index++) {
+    var note = keys_long[(index - 1) % 7];
+    var octave = Math.floor((index - 1) / 7) + start_octave;
+    $('#l'+index+" .key-word-down").text(note+octave);
+    
+}
 /* ------------------------------------------------- */
 
 $(document).keydown(function (event) {
     var x = event.charCode || event.keyCode;
     $("." + x).addClass("button-active");
     var Id = $(("." + x)).attr('id');
-    Id = Id.slice(1);
-    var note = keys[(Id-1)%7];
-    var octave = Math.floor((Id-1)/7) + i;
-    startPlay(note,octave);
+    getNoteOctave(Id);
 });
 
 $(document).keyup(function (event) {
@@ -20,25 +26,35 @@ $(document).keyup(function (event) {
     // stopPlay(note,octave);
 });
 
-$('button').mousedown(function(){
+$('button').mousedown(function () {
     var Id = $(this).attr('id');
-    $('#'+Id).addClass("button-active");
-    Id = Id.slice(1);
-    var note = keys[(Id-1)%7];
-    var octave = Math.floor((Id-1)/7) + i;
-    startPlay(note,octave);
+    $('#' + Id).addClass("button-active");
+    getNoteOctave(Id);
 });
 
-$('button').mouseup(function(){
+$('button').mouseup(function () {
     var Id = $(this).attr('id');
-    $('#'+Id).removeClass("button-active");
+    $('#' + Id).removeClass("button-active");
 });
 
 /* ------------------------------------------------- */
+function getNoteOctave(Id) {
+    var buttonNumber = Id.slice(1);
+    var buttonSize = Id[0];
+    if (buttonSize == 'l') {
+        var note = keys_long[(buttonNumber - 1) % 7];
+        var octave = Math.floor((buttonNumber - 1) / 7) + start_octave;
+    }
+    else{
+        var note = keys_short[(buttonNumber - 1) % 5];
+        var octave = Math.floor((buttonNumber - 1) / 5) + start_octave;
+    }
+    $("#played_key").text(note+octave);
+    startPlay(note, octave);
+}
 
-function startPlay(note,octave) {
-    var piano = Synth.createInstrument('piano');
-    piano.play(note, octave, 2.5); 
+function startPlay(note, octave) {
+    piano.play(note, octave, 2);
 }
 
 function stopPlay(note) {
